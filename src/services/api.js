deb398026dba4743cfe427ef3e18c6d0dd29e483
Karthik-vch <karthikvadlamudi5@gmail.com>
2026-08-19 23:12:01 +0530

    Fix production frontend API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
-const API_BASE_URL = 'http://localhost:5000/api'
-
+const API_BASE_URL = import.meta.env.DEV
+  ? 'http://localhost:5000/api'
+  : '/api'
 export async function getTasks() {
   const response = await fetch(`${API_BASE_URL}/tasks`)
 
